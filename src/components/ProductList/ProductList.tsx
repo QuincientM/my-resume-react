8f6068fe6fd938a11356b083cb0f3c83f205de10
Quincient Madding <quincientmadding@gmail.com>
2025-08-01 19:01:19 -0500

    added productlist updated app and ecom

---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+//import type { Product } from '../data/Product';
+
+const ProductList: React.FC = () => {
+    interface Product {
+     id: string;
+    title: string;
+    image: string;
+    price: number;
+    link: string;
+}
+    const [products, setProducts] = useState<Product[]>([])
+
+    console.log("This is ",products);
+    
+    useEffect(() => {
+        fetch('http://localhost:3000/products')  // JSON file on server
+            .then(res => res.json())     // parse it to JS objects
+            .then((data: Product[]) => {
+                setProducts(data);          // now you have typed JS objects
+                console.log("Products is from useState",data);
+            })
+        .catch(err => console.error(err));
+
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductList;
--- src/pages/Ecom/Ecom.tsx
+++ src/pages/Ecom/Ecom.tsx
@@ -1,5 +1,12 @@
-function Ecom() {
-    return "Ecom";
+import ProductList from "../../components/ProductList/ProductList"
 
+export default function Ecom() {
+    return (
+    <>
+    <h1> E-commerce</h1>
+    <div>
+            <ProductList/>
+    </div>
+    </>
+    )
 }
-export default Ecom
